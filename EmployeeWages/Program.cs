0f207c9dd2afd1dcd7d1ab2428b8b2961d0d8d76
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-10 09:45:02 +0530

    Add project files.

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+    namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int PRESNT = 0;
+            //int partial_present = 1;
+
+            //Random random = new Random();
+            //int empcheck = random.Next(0, 3);
+            //if (empcheck == PRESNT)
+            //{
+            //    Console.WriteLine("employee is present");
+            //}
+            //else if (empcheck == partial_present)
+            //{
+            //    Console.WriteLine("employee is partial present");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("employee is absent");
+            //}
+            int[] dailyWages = new int[5];
+            for (int i = 0; i <= 5; i++)
+            {
+                dailyWages[i] = i;
+            }
+        }
+
+    }
+}
